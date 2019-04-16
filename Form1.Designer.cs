b9b1845212e447857ee9d16c4f62462775312dac
Ponomarov-Vadim <remary300@gmail.com>
2019-04-16 08:32:36 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ProjectX
+{
+    partial class Mineswaeper
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.GameArea = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GameArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameArea
+            // 
+            this.GameArea.AllowUserToAddRows = false;
+            this.GameArea.AllowUserToDeleteRows = false;
+            this.GameArea.AllowUserToResizeColumns = false;
+            this.GameArea.AllowUserToResizeRows = false;
+            this.GameArea.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.GameArea.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.GameArea.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.GameArea.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.GameArea.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.GameArea.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GameArea.ColumnHeadersVisible = false;
+            this.GameArea.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.GameArea.DefaultCellStyle = dataGridViewCellStyle2;
+            this.GameArea.Location = new System.Drawing.Point(133, 27);
+            this.GameArea.MultiSelect = false;
+            this.GameArea.Name = "GameArea";
+            this.GameArea.ReadOnly = true;
+            this.GameArea.RowHeadersVisible = false;
+            this.GameArea.RowHeadersWidth = 40;
+            this.GameArea.RowTemplate.Height = 20;
+            this.GameArea.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.GameArea.Size = new System.Drawing.Size(507, 363);
+            this.GameArea.TabIndex = 0;
+            this.GameArea.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GameArea_CellClick);
+            this.GameArea.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.GameArea_CellValueChanged);
+            // 
+            // Mineswaeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GameArea);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Mineswaeper";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GameArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GameArea;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProjectX
+{
+    public partial class Mineswaeper : Form
+    {
+        public Mineswaeper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                GameArea.Columns.Add(i.ToString(), "");
+            }
+            GameArea.Rows.Add(18);
+
+            for (int i = 0; i < GameArea.ColumnCount; i++)
+            {
+                for (int j = 0; j < GameArea.RowCount; j++)
+                {
+                    GameArea[i, j].Value = 0;
+                }
+            }
+        }
+        private void GameArea_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            GameArea[e.ColumnIndex, e.RowIndex].Value = "X";
+
+            int[][] direct = new int[8][];
+
+            for (int i = 0; i < 8; i++)
+            {
+                direct[i] = new int[2];
+            }
+
+            direct[0][0] = -1; direct[0][1] = -1;
+            direct[1][0] = -1; direct[1][1] = 0;
+            direct[2][0] = -1; direct[2][1] = 1;
+            direct[3][0] = 0; direct[3][1] = -1;
+            direct[4][1] = 0; direct[4][1] = +1;
+            direct[5][0] = 1; direct[5][1] = -1;
+            direct[6][0] = 1; direct[6][1] = 0;
+            direct[7][0] = 1; direct[7][1] = 1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (e.ColumnIndex + direct[i][0] >= 0 && e.RowIndex + direct[i][1] >= 0 &&
+                     e.ColumnIndex + direct[i][0] < GameArea.ColumnCount && e.RowIndex + direct[i][1] < GameArea.RowCount)
+                {
+                    if (GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString() != "X")
+                    {
+                        GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value =
+                        Convert.ToInt32(GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString()) + 1;
+                    }
+                }
+            }
+        }
+
+        public void PushToAreaMines()
+        {
+            int row = 0;
+            int column = 0;
+            Random r = new Random();
+
+            for (int j = 0; j < 86; j++)
+            {
+                int i = r.Next(433);
+                if (i % 24 >= 0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+
+        private Dictionary<int, string> ColorsDictionary = new Dictionary<int, string>()
+        {
+            { 0,"White" },{ 1,"Blue" },{ 2,"Green" },{ 3,"Red" },
+            { 4,"DarkViolet" },{ 5,"DarkOrange" },{ 6,"HotPink" },{ 7,"Brown" },{ 8,"DarkSeaGreen" }
+        };
+
+        private void GameArea_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (GameArea[e.ColumnIndex, e.RowIndex].Value.ToString() != "X")
+            {
+                GameArea[e.ColumnIndex, e.RowIndex].Style.ForeColor =
+                    Color.FromName(ColorsDictionary[Convert.ToInt32(GameArea[e.ColumnIndex, e.RowIndex].Value.ToString())]);
+            }
+            else
+            {
+                GameArea[e.ColumnIndex, e.RowIndex].Style.ForeColor = Color.Black;
+            }
+        }
+    }  //-----------------------------------
+
+}
