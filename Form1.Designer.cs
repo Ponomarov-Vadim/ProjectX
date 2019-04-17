12507ae40606d95d071c5e824a6c899df2f28a24
Ponomarov-Vadim <remary300@gmail.com>
2019-04-17 11:10:50 +0300

    Update files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,16 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.GameArea = new System.Windows.Forms.DataGridView();
+            this.MinesLeft = new System.Windows.Forms.Label();
+            this.MinesCounter = new System.Windows.Forms.Label();
+            this.buttonClearArea = new System.Windows.Forms.Button();
+            this.pictureMines = new System.Windows.Forms.PictureBox();
+            this.buttonFull = new System.Windows.Forms.Button();
+            this.labelOut = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.GameArea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMines)).BeginInit();
             this.SuspendLayout();
             // 
             // GameArea
@@ -63,27 +71,101 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.GameArea.DefaultCellStyle = dataGridViewCellStyle2;
-            this.GameArea.Location = new System.Drawing.Point(133, 27);
+            this.GameArea.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.GameArea.Location = new System.Drawing.Point(181, 27);
             this.GameArea.MultiSelect = false;
             this.GameArea.Name = "GameArea";
             this.GameArea.ReadOnly = true;
             this.GameArea.RowHeadersVisible = false;
             this.GameArea.RowHeadersWidth = 40;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            this.GameArea.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.GameArea.RowTemplate.Height = 20;
             this.GameArea.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.GameArea.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.GameArea.Size = new System.Drawing.Size(507, 363);
             this.GameArea.TabIndex = 0;
             this.GameArea.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GameArea_CellClick);
+            this.GameArea.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.GameArea_CellMouseClick);
             this.GameArea.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.GameArea_CellValueChanged);
+            // 
+            // MinesLeft
+            // 
+            this.MinesLeft.AutoSize = true;
+            this.MinesLeft.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinesLeft.Location = new System.Drawing.Point(31, 44);
+            this.MinesLeft.Name = "MinesLeft";
+            this.MinesLeft.Size = new System.Drawing.Size(90, 21);
+            this.MinesLeft.TabIndex = 1;
+            this.MinesLeft.Text = "Mines Left\r\n";
+            // 
+            // MinesCounter
+            // 
+            this.MinesCounter.AutoSize = true;
+            this.MinesCounter.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinesCounter.Location = new System.Drawing.Point(141, 44);
+            this.MinesCounter.Name = "MinesCounter";
+            this.MinesCounter.Size = new System.Drawing.Size(0, 21);
+            this.MinesCounter.TabIndex = 2;
+            // 
+            // buttonClearArea
+            // 
+            this.buttonClearArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClearArea.Location = new System.Drawing.Point(35, 80);
+            this.buttonClearArea.Name = "buttonClearArea";
+            this.buttonClearArea.Size = new System.Drawing.Size(86, 32);
+            this.buttonClearArea.TabIndex = 3;
+            this.buttonClearArea.Text = "Clear";
+            this.buttonClearArea.UseVisualStyleBackColor = true;
+            this.buttonClearArea.Click += new System.EventHandler(this.ButtonClearArea_Click);
+            // 
+            // pictureMines
+            // 
+            this.pictureMines.BackColor = System.Drawing.Color.Transparent;
+            this.pictureMines.Image = global::ProjectX.Properties.Resources.Mines;
+            this.pictureMines.Location = new System.Drawing.Point(12, 44);
+            this.pictureMines.Name = "pictureMines";
+            this.pictureMines.Size = new System.Drawing.Size(15, 15);
+            this.pictureMines.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureMines.TabIndex = 4;
+            this.pictureMines.TabStop = false;
+            // 
+            // buttonFull
+            // 
+            this.buttonFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFull.Location = new System.Drawing.Point(35, 134);
+            this.buttonFull.Name = "buttonFull";
+            this.buttonFull.Size = new System.Drawing.Size(86, 32);
+            this.buttonFull.TabIndex = 5;
+            this.buttonFull.Text = "Full";
+            this.buttonFull.UseVisualStyleBackColor = true;
+            this.buttonFull.Click += new System.EventHandler(this.ButtonFull_Click);
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOut.Location = new System.Drawing.Point(12, 202);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(19, 21);
+            this.labelOut.TabIndex = 6;
+            this.labelOut.Text = "0";
             // 
             // Mineswaeper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.buttonFull);
+            this.Controls.Add(this.pictureMines);
+            this.Controls.Add(this.buttonClearArea);
+            this.Controls.Add(this.MinesCounter);
+            this.Controls.Add(this.MinesLeft);
             this.Controls.Add(this.GameArea);
             this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Mineswaeper";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -91,13 +173,21 @@
             this.Text = "Minesweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.GameArea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMines)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView GameArea;
+        private System.Windows.Forms.Label MinesLeft;
+        private System.Windows.Forms.Label MinesCounter;
+        private System.Windows.Forms.Button buttonClearArea;
+        private System.Windows.Forms.PictureBox pictureMines;
+        private System.Windows.Forms.Button buttonFull;
+        private System.Windows.Forms.Label labelOut;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ProjectX
@@ -20,18 +21,66 @@ namespace ProjectX
             }
             GameArea.Rows.Add(18);
 
+            FillEmptyValueGameArea();
+        }
+
+        private void FillEmptyValueGameArea()
+        {
+            MinesCounter.Text = "9"; // 86
             for (int i = 0; i < GameArea.ColumnCount; i++)
             {
                 for (int j = 0; j < GameArea.RowCount; j++)
                 {
-                    GameArea[i, j].Value = 0;
+                    GameArea[i, j].Value = "";
                 }
             }
         }
+
         private void GameArea_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            GameArea[e.ColumnIndex, e.RowIndex].Value = "X";
 
+            //FillMethod(e.RowIndex, e.ColumnIndex);
+
+            //int[][] direct = new int[8][];
+
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    direct[i] = new int[2];
+            //}
+
+            //direct[0][0] = -1; direct[0][1] = -1;
+            //direct[1][0] = -1; direct[1][1] = 0;
+            //direct[2][0] = -1; direct[2][1] = 1;
+            //direct[3][0] = 0; direct[3][1] = -1;
+            //direct[4][1] = 0; direct[4][1] = +1;
+            //direct[5][0] = 1; direct[5][1] = -1;
+            //direct[6][0] = 1; direct[6][1] = 0;
+            //direct[7][0] = 1; direct[7][1] = 1;
+
+            //if (Convert.ToInt32(MinesCounter.Text) > 0 && GameArea[e.ColumnIndex, e.RowIndex].Value.ToString() != "X")
+            //{
+            //    GameArea[e.ColumnIndex, e.RowIndex].Style.ForeColor = Color.Black;
+            //    GameArea[e.ColumnIndex, e.RowIndex].Value = "X";
+
+            //    MinesCounter.Text = (Convert.ToInt32(MinesCounter.Text) - 1).ToString(); 
+
+            //    for (int i = 0; i < 8; i++)
+            //    {
+            //        if (e.ColumnIndex + direct[i][0] >= 0 && e.RowIndex + direct[i][1] >= 0 &&
+            //             e.ColumnIndex + direct[i][0] < GameArea.ColumnCount && e.RowIndex + direct[i][1] < GameArea.RowCount)
+            //        {
+            //            if (GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString() != "X")
+            //            {
+            //                GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value =
+            //                Convert.ToInt32(GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString()) + 1;
+            //            }
+            //        }
+            //    }
+            //}
+        }
+
+        public void FillMethod(int RowIndex, int ColumnIndex)
+        {
             int[][] direct = new int[8][];
 
             for (int i = 0; i < 8; i++)
@@ -48,39 +97,87 @@ namespace ProjectX
             direct[6][0] = 1; direct[6][1] = 0;
             direct[7][0] = 1; direct[7][1] = 1;
 
+            GameArea[ColumnIndex, RowIndex].Style.ForeColor = Color.Black;
+            GameArea[ColumnIndex, RowIndex].Value = "X";
+
+
             for (int i = 0; i < 8; i++)
             {
-                if (e.ColumnIndex + direct[i][0] >= 0 && e.RowIndex + direct[i][1] >= 0 &&
-                     e.ColumnIndex + direct[i][0] < GameArea.ColumnCount && e.RowIndex + direct[i][1] < GameArea.RowCount)
+                if (ColumnIndex + direct[i][0] >= 0 && RowIndex + direct[i][1] >= 0 &&
+                     ColumnIndex + direct[i][0] < GameArea.ColumnCount && RowIndex + direct[i][1] < GameArea.RowCount)
                 {
-                    if (GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString() != "X")
+                    if (GameArea[ColumnIndex + direct[i][0], RowIndex + direct[i][1]].Value.ToString() != "X")
                     {
-                        GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value =
-                        Convert.ToInt32(GameArea[e.ColumnIndex + direct[i][0], e.RowIndex + direct[i][1]].Value.ToString()) + 1;
+                        if (GameArea[ColumnIndex + direct[i][0], RowIndex + direct[i][1]].Value.ToString()=="")
+                        {
+                            GameArea[ColumnIndex + direct[i][0], RowIndex + direct[i][1]].Value = 1;
+                        }
+                        else
+                        {
+                            GameArea[ColumnIndex + direct[i][0], RowIndex + direct[i][1]].Value =
+                            Convert.ToInt32(GameArea[ColumnIndex + direct[i][0], RowIndex + direct[i][1]].Value.ToString()) + 1;
+                        }
                     }
                 }
             }
+
         }
 
         public void PushToAreaMines()
         {
             int row = 0;
             int column = 0;
-            Random r = new Random();
+            Random rand = new Random();
 
             for (int j = 0; j < 86; j++)
             {
-                int i = r.Next(433);
-                if (i % 24 >= 0)
+                labelOut.Text = (Convert.ToInt32(labelOut.Text.ToString()) + 1).ToString();
+                int r = rand.Next(433);                
+                if (r < 24)
                 {
-
+                    row = 0;
+                    column = r;
                 }
                 else
                 {
+                    if (((r / 24) - 1) > 0)
+                    {
+                        row = (r / 24) -1  ;
+                    }
+                    else
+                    {
+                        row = 0;
+                    }
+                    if (((r % 24) - 1) > 0) //--
+                    {
+                        column = (r % 24) ;//--
+                    }
+                    else
+                    {
+                        column = 0;
+                    }
+                }
+                if (GameArea[column, row].Value.ToString() != "X")
+                {
+                    writeouts(row, column, r);
+                    FillMethod(row, column);
+                }
+                else
+                {
+                    j--;
 
                 }
             }
+        }
 
+        public void writeouts(int row, int colum, int rand)
+        {
+            string str = "Rows num =" + row + "; Colums num = " + colum + "; Rand num = " + rand + ";\n";
+            using (StreamWriter s = new StreamWriter("info.txt", true))
+            {
+
+                s.WriteLine(str);
+            }
         }
 
         private Dictionary<int, string> ColorsDictionary = new Dictionary<int, string>()
@@ -91,7 +188,8 @@ namespace ProjectX
 
         private void GameArea_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            if (GameArea[e.ColumnIndex, e.RowIndex].Value.ToString() != "X")
+            if (GameArea[e.ColumnIndex, e.RowIndex].Value.ToString() != "X" &&
+                !string.IsNullOrEmpty(GameArea[e.ColumnIndex, e.RowIndex].Value.ToString())) //------------
             {
                 GameArea[e.ColumnIndex, e.RowIndex].Style.ForeColor =
                     Color.FromName(ColorsDictionary[Convert.ToInt32(GameArea[e.ColumnIndex, e.RowIndex].Value.ToString())]);
@@ -101,6 +199,22 @@ namespace ProjectX
                 GameArea[e.ColumnIndex, e.RowIndex].Style.ForeColor = Color.Black;
             }
         }
-    }  //-----------------------------------
 
+        private void ButtonClearArea_Click(object sender, EventArgs e)
+        {
+            FillEmptyValueGameArea();
+        }
+
+        private void GameArea_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //MouseButtons.
+            //e.Button.
+        }
+
+        private void ButtonFull_Click(object sender, EventArgs e)
+        {
+            PushToAreaMines();
+        }
+    }
+    //-----------------------------------
 }
